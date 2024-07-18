23d249d7109e275d2f877c652b504391ce7421e4
koogw <156035556+koogw@users.noreply.github.com>
2024-07-18 16:02:11 +0900

    Create snake.js

---
+++ js/snake.js
@@ -0,0 +1,38 @@
+const canvas = document.getElementById("game-board");
+const ctx = canvas.getContext("2d");
+const scale = 20;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+let score = 0;
+
+let snake;
+
+(function setup() {
+  snake = new Snake();
+  fruit = new Fruit();
+
+  fruit.pickLocation();
+
+  window.setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    fruit.draw();
+    snake.update();
+    snake.draw();
+
+    if (snake.eat(fruit)) {
+      score++;
+      document.getElementById("score").textContent = score;
+      fruit.pickLocation();
+    }
+
+    snake.checkCollision();
+  }, 250);
+}());
+
+function Snake() {
+  // ...
+}
+
+function Fruit() {
+  // ...
+}
